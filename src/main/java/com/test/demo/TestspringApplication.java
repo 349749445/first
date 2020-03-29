71b25824a547f0792886d665832ec5c90a59bf20
Your Name <you@example.com>
2020-03-29 12:23:21 +0800

    测试redis缓存，配置自动转json的cachemanager

--- src/main/java/com/test/demo/TestspringApplication.java
+++ src/main/java/com/test/demo/TestspringApplication.java
@@ -3,11 +3,11 @@ package com.test.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.cache.annotation.EnableCaching;
 
 @MapperScan(value = "com.test.demo.**.dao")
 @SpringBootApplication
+@EnableCaching
 public class TestspringApplication {
 
 	public static void main(String[] args) {
